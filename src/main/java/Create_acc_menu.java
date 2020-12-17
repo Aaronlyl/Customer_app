0a04b7e9d0022100c98db41b8acec8957c4ebbbf
Aaronlyl <leeaaronyl@gmail.com>
2020-12-17 01:27:18 +0000

    fix

--- src/main/java/Create_acc_menu.java
+++ src/main/java/Create_acc_menu.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionListener;
 
 public class Create_acc_menu extends JPanel{
     //Probably a more secure idea to remove variables later and directly upload to servlet
-    private String usr, pw, c_pw;
+    private String usr, pw;
     private JButton register_btn;
     private JTextField usr_tfield, pw_tfield, c_pw_tfield ;
     private JLabel check = new JLabel();
@@ -16,24 +16,27 @@ public class Create_acc_menu extends JPanel{
         JLabel title = new JLabel("PHAB Pharmacy Account Registration");
         Color title_col = Color.decode("#3E5641");
         title.setForeground(title_col);
-        usr_tfield = new JTextField("1234");
-        pw_tfield = new JTextField("1234");
-        c_pw_tfield = new JTextField("1234");
+        check.setForeground(Color.red);
+        usr_tfield = new JTextField();
+        pw_tfield = new JTextField();
+        c_pw_tfield = new JTextField();
         register_btn = new JButton("Register");
 
         register_btn.addActionListener(new ActionListener() {
             //Add implementation of database here
             @Override
             public void actionPerformed(ActionEvent e) {
-                usr = usr_tfield.getText();
-                pw = pw_tfield.getText();
-                c_pw = c_pw_tfield.getText();
 
                 boolean create_acc = false;
                 String checktest;
-                if(pw.equals(c_pw) && usr_tfield.getText().isEmpty()==false ) {
-                    checktest =  "Account Created" ;
-                    create_acc = true;
+
+                if(pw_tfield.getText().equals(c_pw_tfield.getText()) && usr_tfield.getText().isEmpty()==false ) {
+                    if(pw_tfield.getText().length() < 8 ) checktest = "Password length must be longer than 8 characters. Please retry.";
+                    else {
+                        checktest =  "Account Created" ;
+                        create_acc = true;
+                        check.setForeground(Color.green);
+                    }
                     
 
 
@@ -46,9 +49,12 @@ public class Create_acc_menu extends JPanel{
 
                 }
                 if(create_acc) {
-                    
+                    //Add implementation of database here
+                    usr = usr_tfield.getText();
+                    pw = pw_tfield.getText();
                 }
                 check.setText(checktest);
+                
                 revalidate();
             }
 
