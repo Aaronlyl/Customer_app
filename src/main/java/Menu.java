9975e3588b13966e5b318fed85fc165cf134917c
panchanit18 <pt218@ic.ac.uk>
2021-01-10 10:19:48 +0700

    select branch

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Menu extends JPanel{
 
@@ -22,6 +24,38 @@ public class Menu extends JPanel{
         branchPanel = new JPanel();
         branchPanel.setLayout(new GridLayout(2,1));
         branchPanel.add(branch_Selector);
+        ActionListener branchSelectorActionListener = new ActionListener() {//add actionlistener to listen for change
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String s = (String) branch_Selector.getSelectedItem();//get the selected item
+                String Branch1 = "Paddington";
+                String Branch2 = "MileEnd";
+                String Branch3 = "GreenPark";
+
+                switch (s) {//check for a match
+                    case "Green Park":
+                        POST_Requests P = new POST_Requests(Branch3,"https://phabservlet1.herokuapp.com/inputB");
+                        System.out.println("Branch selected: Green Park");
+
+                        break;
+                    case "Paddington":
+                        POST_Requests P2 = new POST_Requests(Branch1,"https://phabservlet1.herokuapp.com/inputB");
+                        System.out.println("Branch selected: Paddington");
+
+                        break;
+                    case "Mile End":
+                        POST_Requests P3 = new POST_Requests(Branch2,"https://phabservlet1.herokuapp.com/inputB");
+                        System.out.println("Branch selected: Mile End");
+                        break;
+                    default:
+                        System.out.println("No branch selected, please select a branch");
+                        break;
+                }
+            }
+        };
+
+        branch_Selector.addActionListener(branchSelectorActionListener);
         branchPanel.add(new JTextArea("%Availabillty and Price listed here%"));
         storePanel.add(branchPanel);
 
